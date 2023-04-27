555d3b00d20305604780bea4e13a7b0cdfda2fe4
Eric <58400036+echeung52@users.noreply.github.com>
2023-04-26 21:11:59 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -6,51 +6,45 @@ let ms = 0;
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
-let totalTime=0;
+let totalTime = 0;
 let interval;
 
-startButton.onclick = function(){
-    clearInterval(interval);
-    interval=setInterval(start, 10);
+startButton.onclick = function () {
+  clearInterval(interval);
+  interval = setInterval(start, 10);
+};
+
+stopButton.onclick = function () {
+  clearInterval(interval);
+};
+
+resetButton.onclick = function () {
+  clearInterval(interval);
+  time.innerHTML = "00:00:00:00";
+  ms = 0;
+  seconds = 0;
+  minutes = 0;
+  hours = 0;
+  totalTime = 0;
+};
+
+function start() {
+  totalTime++;
+  updateScreen();
 }
 
-stopButton.onclick = function(){
-    clearInterval(interval);
+function updateScreen() {
+  ms = totalTime % 100;
+  seconds = pad(Math.floor((totalTime / 100) % 60));
+  minutes = pad(Math.floor((totalTime / (100 * 60)) % 60));
+  hours = pad(Math.floor(totalTime / (100 * 60 * 60)));
+
+  time.innerHTML = `${hours}:${minutes}:${seconds}:${ms}`;
 }
 
-resetButton.onclick = function(){
-    clearInterval(interval);
-    time.innerHTML="00:00:00:00";
-    ms = 0;
-    seconds = 0;
-    minutes = 0;
-    hours = 0;
-    totalTime = 0;
-}
-
-function start(){
-    totalTime++;
-    updateScreen();
-}
-
-function updateScreen(){
-    ms = totalTime%100;
-    seconds = Math.floor((totalTime/100)%60);
-    minutes = Math.floor((totalTime/(100*60))%60);
-    hours = Math.floor(totalTime/(100*60*60));
-
-    ms = pad(ms);
-    seconds = pad(seconds);
-    minutes = pad(minutes);
-    hours = pad(hours);
-
-    time.innerHTML = `${hours}:${minutes}:${seconds}:${ms}`;
-
-}
-
-function pad(unit){
-    if(unit<10){
-        return ("0"+unit);
-    }
-        return unit;
+function pad(unit) {
+  if (unit < 10) {
+    return "0" + unit;
+  }
+  return unit;
 }
